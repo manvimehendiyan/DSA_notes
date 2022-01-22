2a9faa00cfe1e4b6bee8fe1b5266d467261cd606
Manvi Mehendiyan <79982384+manvimehendiyan@users.noreply.github.com>
2022-01-22 16:46:19 +0530

    Add files via upload

---
+++ Optimized_BubbleSort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+void swap(int *x,int *y)
+{
+	int temp=*x;
+	*x=*y;
+	*y=temp;
+}
+int bubble_sort(int a[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		int flag=0;
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				flag=1;
+				swap(&a[j],&a[j+1]);
+			}
+		}
+		if(flag==0)
+        	break;
+	}
+}
+int main()
+{
+	int n,num;
+	cout<<"Enter total numbers: ";
+	cin>>n;
+	int a[n];
+	cout<<"Enter the array: "<<endl;
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	bubble_sort(a,n);
+	cout<<"Sorted Array: ";
+	for(int i=0;i<n;i++)
+		cout<<a[i]<<" ";
+	cout<<endl;
+	return 0;
+}
